d32e1bc64dce7bb7bedbd52055168b08aea61fa9
David Huchim <32229063+dhuchim@users.noreply.github.com>
2018-10-17 10:52:10 -0500

    Add files via upload

--- ejem810.m
+++ ejem810.m
@@ -2,12 +2,20 @@
 clc;clear all;close all;
 c = [1,1]; 
 d = [1,5,6];
-T = 0.1; 
+T = 0.1;Fs=1/T;
+t=0:0.1:3;
 [b,a] = imp_invr(c,d,T) 
 [C,B,A] = sdir2cas(b,a) 
-t=0:0.01:3;
 [ha,x,t] =impulse(c,d,t);
-[db,mag,pha,grd,w] = freqz_m(b,a);
-[dba,maga,phaa,wa] = freqs_m(b,a,pi);
-subplot(2,1,2); plot(w,mag);title('mag');hold on;axis([0 10 0 3]);
+subplot(2,1,1);plot(t,ha );hold on;stem(t,ha,'b');axis([0 3 -0.1 1]);
+title('Impulse Responses');xlabel('time in sec');ylabel('Amplitude');
+[db,magd,pha,grd,wd] = freqz_m(b,a);
+[db,mags,pha,ws] = freqs_m(c,d,2*pi*Fs);
+subplot(2,1,2); plot(ws/(2*pi),mags*Fs,wd/(2*pi)*Fs,magd,'b')
+xlabel('frequency in Hz'); title('Magnitude Responses');
+ylabel('Magnitude'); text(1.4,.5,'Analog filter'); text(1.5,1.5,'Digital filter')
+
+
+
+
 
--- ejem811.m
+++ ejem811.m
@@ -1,35 +1,27 @@
 clc
 clear all
 close all
-wp = 0.2*pi; % digital Passband freq in Hz
-ws = 0.3*pi; % digital Stopband freq in Hz
-Rp = 1; % Passband ripple in dB
-As = 15; % Stopband attenuation in dB 
-T = 1; % Set T=1
-OmegaP = wp / T; % Prototype Passband freq
-OmegaS = ws / T; % Prototype Stopband freq
+wp = 0.2*pi; 
+ws = 0.3*pi; 
+Rp = 1; 
+As = 15; 
+T = 1; 
+OmegaP = wp / T; 
+OmegaS = ws / T; 
 [cs,ds] = afd_butt(OmegaP,OmegaS,Rp,As);
 [b,a] = imp_invr(cs,ds,T); 
 [C,B,A] = dir2par(b,a)
 [db,mag,pha,grd,w] = freqz_m(b,a);
-
 figure('Name','Ejemplo 8.11','Numbertitle','off');
-subplot(2,2,1); plot(w/pi,mag);title('Magnitude Response');axis([0 1 0 1.1]);
-xticks([0 0.2 0.3 1]);yticks([0 0.1778 0.8913 1]);%lineas de guia
+subplot(2,2,1); plot(w/pi,mag);title('Magnitude Response');grid on;
+axis([0 1 0 1.1]);xticks([0 0.2 0.3 1]);yticks([0 0.1778 0.8913 1]);
 ylabel('|H|');xlabel('frequency in pi units');
-grid on;
-
-subplot(2,2,2); plot(w/pi,pha/pi);title('Phase Response');axis([0 1 -1 1]);
-xticks([0 0.2 0.3 1]);yticks([0]);%lineas de guia
+subplot(2,2,2); plot(w/pi,pha/pi);title('Phase Response');grid on;
+axis([0 1 -1 1]);xticks([0 0.2 0.3 1]);yticks([0]);
 ylabel('pi units');xlabel('frequency in pi units');
-grid on;
-
-subplot(2,2,3); plot(w/pi,db);title('Magnitude in dB');axis([0 1 -40 2]);
-xticks([0 0.2 0.3 1]);yticks([-15 -1 0]);%lineas de guia
+subplot(2,2,3); plot(w/pi,db);title('Magnitude in dB');grid on;
+axis([0 1 -40 2]);xticks([0 0.2 0.3 1]);yticks([-15 -1 0]);
 ylabel('decibels');xlabel('frequency in pi units');
-grid on;
-
-subplot(2,2,4); plot(w/pi,grd);title('Group Delay');
-xticks([0 0.2 0.3 1]);yticks([0 2 4 6 8 10]);%lineas de guia
+subplot(2,2,4); plot(w/pi,grd);title('Group Delay');grid on;
+xticks([0 0.2 0.3 1]);yticks([0 2 4 6 8 10]);
 ylabel('Samples');xlabel('frequency in pi units');
-grid on;
